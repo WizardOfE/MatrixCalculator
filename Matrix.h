2fe7211aa00e4c55bdd9822d25e5273a754da782
equirie24@georgefox.edu <equirie24@georgefox.edu>
2026-04-25 22:45:57 -0700

    added default constructor

--- Matrix.h
+++ Matrix.h
@@ -9,7 +9,18 @@ private:
   int _cols;
 
 public:
-  Matrix(int rows, int cols, int **data) : _data(data), _rows(rows), _cols(cols) {};
+  Matrix(int rows, int cols, int **data)
+      : _data(data), _rows(rows), _cols(cols) {};
+  Matrix(Matrix &matrix)
+      : _data(matrix._data), _rows(matrix._rows), _cols(matrix._cols) {};
+  Matrix() {
+    _rows = 2;
+    _cols = 2;
+    _data = new int *[_rows];
+    for (int x = 0; x < _rows; x++) {
+      _data[x] = new int[_cols];
+    }
+  };
   ~Matrix();
   Matrix &operator+(Matrix &other);
   Matrix &operator+=(Matrix &other);
@@ -18,12 +29,8 @@ public:
   Matrix &operator*(Matrix &other);
   Matrix &operator*=(Matrix &other);
 
-  int getRows() {
-    return _rows;
-  }
-  int gowCol() {
-    return _cols;
-  }
+  int getRows() { return _rows; }
+  int gowCol() { return _cols; }
 
   friend bool operator==(const Matrix &lhs, const Matrix &rhs);
   friend std::ostream &operator<<(std::ostream &os, const Matrix &matrix);
