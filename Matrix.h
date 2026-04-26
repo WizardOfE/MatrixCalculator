74616cf17a41e5268749f9858c9e550452ec3cd5
obrsb <linterianuc23@georgefox.edu>
2026-04-25 23:38:19 -0700

    Reworked Calculator

--- Matrix.h
+++ Matrix.h
@@ -11,8 +11,38 @@ private:
 public:
   Matrix(int rows, int cols, int **data)
       : _data(data), _rows(rows), _cols(cols) {};
-  Matrix( const Matrix &matrix)
-      : _data(matrix._data), _rows(matrix._rows), _cols(matrix._cols) {};
+  Matrix(const Matrix &other) {
+    _rows = other._rows;
+    _cols = other._cols;
+
+    _data = new int*[_rows];
+    for (int i = 0; i < _rows; i++) {
+      _data[i] = new int[_cols];
+      for (int j = 0; j < _cols; j++) {
+        _data[i][j] = other._data[i][j];
+      }
+    }
+  }
+
+  Matrix& operator=(const Matrix &other) {
+    if (this == &other) return *this;
+
+    for (int i = 0; i < _rows; i++) delete[] _data[i];
+    delete[] _data;
+
+    _rows = other._rows;
+    _cols = other._cols;
+
+    _data = new int*[_rows];
+    for (int i = 0; i < _rows; i++) {
+      _data[i] = new int[_cols];
+      for (int j = 0; j < _cols; j++) {
+        _data[i][j] = other._data[i][j];
+      }
+    }
+    return *this;
+  }
+
   Matrix() {
     _rows = 2;
     _cols = 2;
